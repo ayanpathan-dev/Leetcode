046b92ac8ff9e57e76272e1e73fc114f6a832e35
ayanpathan-dev <ayanpathan1500@gmail.com>
2026-04-20 21:06:42 +0530

    added

--- Array/MaximumSumWithExactlyKElements2656.java
+++ Array/MaximumSumWithExactlyKElements2656.java
@@ -1,5 +1,17 @@
+import java.util.Arrays;
+
 public class MaximumSumWithExactlyKElements2656 {
-    public int maximizeSum(int[] nums, int k) {
-        return
+   class Solution {
+   public int maximizeSum(int[] nums, int k) {
+        int n =nums.length-1;
+        int sum =0;
+        Arrays.sort(nums); 
+        for(int i =0;i<k;i++){
+            sum+=nums[n];
+            nums[n]+=1;
+        }
+        
+        return sum;
     }
+}
 }
